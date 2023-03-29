4c0e118b8ae8cc923ca79d9a0725259684f08d23
alxvth <a.vieth@tudelft.nl>
2023-03-29 12:17:16 +0200

    Fix cmake variable use avx

--- cmake/CMakeCommon.cmake
+++ cmake/CMakeCommon.cmake
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 # Check for and link to AVX instruction sets
 # -----------------------------------------------------------------------------
-macro(check_and_link_AVX target)
+macro(check_and_link_AVX target useavx)
 	message(STATUS "Set instruction sets for ${target}")
 	
 	# Use cmake hardware checks to see whether AVX should be activated
@@ -15,10 +15,10 @@ macro(check_and_link_AVX target)
 	check_cxx_compiler_flag(${AXV_CompileOption} COMPILER_OPT_AVX_SUPPORTED)
 	check_cxx_compiler_flag(${AXV2_CompileOption} COMPILER_OPT_AVX2_SUPPORTED)
 
-	if(${USE_AVX} AND ${COMPILER_OPT_AVX2_SUPPORTED})
+	if(${useavx} AND ${COMPILER_OPT_AVX2_SUPPORTED})
 		MESSAGE( STATUS "Use AXV2")
 		target_compile_options(${target} PRIVATE ${AXV2_CompileOption})
-	elseif(${USE_AVX} AND ${COMPILER_OPT_AVX_SUPPORTED})
+	elseif(${useavx} AND ${COMPILER_OPT_AVX_SUPPORTED})
 		MESSAGE( STATUS "Use AXV")
 		target_compile_options(${target} PRIVATE ${AXV_CompileOption})
 	else()
