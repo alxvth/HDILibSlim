3924edc322fe9962d13495a21b28a8ab3917aea1
alxvth <a.vieth@tudelft.nl>
2023-03-29 12:37:39 +0200

    cmake fix for CI

--- cmake/CMakeCommon.cmake
+++ cmake/CMakeCommon.cmake
@@ -2,12 +2,12 @@
 # Check for and link to AVX instruction sets
 # -----------------------------------------------------------------------------
 macro(check_and_link_AVX target useavx)
-	message(STATUS "Set instruction sets for ${target}")
+	message(STATUS "Set instruction sets for ${target}, useavx is ${useavx}")
 	
 	# Use cmake hardware checks to see whether AVX should be activated
 	include(CheckCXXCompilerFlag)
 
-	# Hardware accelations: SSE and AVX
+	# Hardware accelerations: SSE and AVX
 	set(AXV_CompileOption $<IF:$<CXX_COMPILER_ID:MSVC>,/arch:AVX,-DUSE_AVX>)
 	set(AXV2_CompileOption $<IF:$<CXX_COMPILER_ID:MSVC>,/arch:AVX2,-DUSE_AVX2>)
 	set(SSE2_CompileOption $<IF:$<CXX_COMPILER_ID:MSVC>,/arch:SSE2,-DUSE_SSE2>)
@@ -15,12 +15,17 @@ macro(check_and_link_AVX target useavx)
 	check_cxx_compiler_flag(${AXV_CompileOption} COMPILER_OPT_AVX_SUPPORTED)
 	check_cxx_compiler_flag(${AXV2_CompileOption} COMPILER_OPT_AVX2_SUPPORTED)
 
-	if(${useavx} AND ${COMPILER_OPT_AVX2_SUPPORTED})
-		MESSAGE( STATUS "Use AXV2")
-		target_compile_options(${target} PRIVATE ${AXV2_CompileOption})
-	elseif(${useavx} AND ${COMPILER_OPT_AVX_SUPPORTED})
-		MESSAGE( STATUS "Use AXV")
-		target_compile_options(${target} PRIVATE ${AXV_CompileOption})
+	if(${useavx})
+		if(${COMPILER_OPT_AVX2_SUPPORTED})
+			MESSAGE( STATUS "Use AXV2")
+			target_compile_options(${target} PRIVATE ${AXV2_CompileOption})
+		elseif(${COMPILER_OPT_AVX_SUPPORTED})
+			MESSAGE( STATUS "Use AXV")
+			target_compile_options(${target} PRIVATE ${AXV_CompileOption})
+		else()
+			message(STATUS "USE_AVX is ON but no AVX hardware acceleration available, default to SSE2")
+			target_compile_options(${target} PRIVATE ${SSE2_CompileOption})
+		endif()
 	else()
 		MESSAGE( STATUS "Use SSE2")
 		target_compile_options(${target} PRIVATE ${SSE2_CompileOption})
