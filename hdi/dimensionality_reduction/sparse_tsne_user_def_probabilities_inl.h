6fbda2e5061780986e9a55e11f81876737707a34
Alexander Vieth <a.vieth@tudelft.nl>
2024-04-22 13:35:17 +0200

    More uniform API and initial embedding fixes (#35)
    * Some fixes and convenience functions
    
    * Formatting
    
    * Add _presetEmbedding option to tsne parameters
    
    * Fix: Use preset embedding

--- hdi/dimensionality_reduction/sparse_tsne_user_def_probabilities_inl.h
+++ hdi/dimensionality_reduction/sparse_tsne_user_def_probabilities_inl.h
@@ -107,7 +107,10 @@ namespace hdi{
       utils::secureLogValue(_logger,"Number of data points",_P.size());
 
       computeHighDimensionalDistribution(probabilities);
-      initializeEmbeddingPosition(params._seed, params._rngRange);
+
+      if (!params._presetEmbedding) {
+        initializeEmbeddingPosition(_params._seed, _params._rngRange);
+      }
 
       _iteration = 0;
 
@@ -136,7 +139,10 @@ namespace hdi{
       utils::secureLogValue(_logger,"Number of data points",_P.size());
 
       _P = distribution;
-      initializeEmbeddingPosition(params._seed, params._rngRange);
+
+      if (!params._presetEmbedding) {
+        initializeEmbeddingPosition(_params._seed, _params._rngRange);
+      }
 
       _iteration = 0;
 
